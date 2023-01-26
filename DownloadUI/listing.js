a42d3b988170ff87c6a287e4d5d5a69dc42148c6
phenix.html <73414034+996Jack@users.noreply.github.com>
2023-01-26 19:38:12 +1100

    Attempt of DownloadUI

--- DownloadUI/listing.js
+++ DownloadUI/listing.js
@@ -1,21 +1,54 @@
-{
-    "macOS": {
-        "macOS Monterey Retail": "https://bit.ly/MontereyISO",
-        "macOS Monterey Beta": "https://www.mediafire.com/file/9bg8p55zgfkzd4n/macOS+12-+21A5248p.iso/file",
-        "macOS Big Sur": "https://bit.ly/2VatU7J",
-        "macOS Catalina": "https://bit.ly/3ljdtk2",
-        "macOS Mojave": "https://bit.ly/2J7NzmB",
-        "macOS Sierra": "https://bit.ly/2LZqWl2",
-    },
-    "Rufus": {
-        "1.0.3": "https://terabox.com/s/1mvCkXT47hp2lKpY3RkmYQg",
-        "3.3": "https://terabox.com/s/17SZbJTAtafFE4ghKpnyiBA",
-        "3.19": "https://terabox.com/s/1R9mZTvGWqe9_Bhev1yb9cQ",
-        "3.21": "https://terabox.com/s/1glHJJj6PKorf3CsSXHsmtw",
-        "3.20": "https://terabox.com/s/1UOo1l42RTYE5Sg4fhESstw",
-        "3.2": "https://terabox.com/s/15Aq6I9E1ah4U-rMBreYopg",
-    },
-    "VMware": {
-        "15": "https://terabox.com/s/1xIo-9QU-A1CwSD34ts8u-g",
-    },
-}
+let productList = {};
+
+const typeObj = document.querySelector("#type");
+const versionObj = document.querySelector("#version");
+const editionObj = document.querySelector("#edition");
+const downloadURL = document.querySelector("#download");
+
+fetch("/products.json").then(data => data.json().then(products => {
+	productList = products;
+	for (const i in products) {
+		typeObj.append(new Option(i, i));
+	}
+}));
+
+const updateVersions = () => {
+	const versionOptions = document.querySelectorAll("#version option");
+
+	for (const i of versionOptions) {
+		if (!i.disabled) i.remove();
+	}
+
+	document.querySelector("#version option[value='placeholder']").selected = true;
+
+	for (const i in productList[typeObj.value]) {
+		versionObj.append(new Option(i, i));
+	}
+
+	updateEditions();
+};
+
+const updateEditions = () => {
+	let editionOptions = document.querySelectorAll("#edition option");
+	for (let i of editionOptions) {
+		if (!i.disabled) i.remove();
+	}
+
+	document.querySelector("#edition option[value='placeholder']").selected = true;
+
+	for (let i in productList[typeObj.value][versionObj.value]) {
+		editionObj.append(new Option(i, i));
+	}
+
+	updateURL();
+};
+
+const updateURL = () => {
+	if (typeObj.value && versionObj.value && editionObj.value) {
+		const url = productList[typeObj.value][versionObj.value][editionObj.value];
+		downloadURL.href = url;
+	} else {
+		downloadURL.removeAttr("href");
+		downloadURL.disabled = true;
+	}
+};
